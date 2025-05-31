7305d04cd2f79048095b224bbdf0edba3413737a
selestino <selestinoolivier@gmail.com>
2025-05-31 14:28:46 +0300

    @selestinohajaniaina modify app ID

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.bus-mj',
-  appName: 'Bus Nakay',
+  appId: 'io.ionic.bus-nakay',
+  appName: 'bus nakay',
   webDir: 'www',
   server: {
     androidScheme: 'https'
