ee96490710e5a1ec3574b9d432b1a8855ec2892d
selestino <selestinoolivier@gmail.com>
2026-02-13 01:41:41 +0300

    @selestinohajaniaina add cooperative liste

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,27 +1,44 @@
 import { Component } from '@angular/core';
 import { Bus } from '../interface/bus';
-import { findBusAll } from 'bus-mj';
+import { findBusAll, findOperatorAll, findBusDetailByOperator } from 'bus-mj';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
 })
 export class Tab1Page {
-
   public allBus: Bus[];
+  public allCooperative: string[];
   public chargeShow: boolean = true;
   public Bus: Bus;
+  public ionColor = [
+    'tertiary',
+    'medium',
+    'danger',
+    'warning',
+    'dark',
+    'primary',
+    'secondary',
+    'success',
+  ];
 
   constructor() {}
 
   ngOnInit() {
     this.allBus = findBusAll();
+    this.allCooperative = findOperatorAll();
     setTimeout(() => {
-      this.chargeShow = this.allBus.length === 0 ? true : false;    
+      this.chargeShow = this.allBus.length === 0 ? true : false;
     }, 1000);
-    this.allBus.map(bus => console.log(bus.tags.color));
-    
+    this.allBus.map((bus) => console.log(bus.tags.color));
   }
 
+  getBusByCooperative(coop: string) {
+    return findBusDetailByOperator(coop).length;
+  }
+
+  getBusColor(position: number) {
+    return this.ionColor[position % this.ionColor.length];
+  }
 }
