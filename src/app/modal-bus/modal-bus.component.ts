7e5c71527032318b2fd742633dd33ea7abbcde65
selestino <selestinoolivier@gmail.com>
2026-07-19 15:13:33 +0300

    @selestinohajaniaina show bus modal detail

--- src/app/modal-bus/modal-bus.component.ts
+++ src/app/modal-bus/modal-bus.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnInit, ViewChild } from '@angular/core';
-// import { Bus } from '../interface/bus';
+import { Bus } from '../interface/bus';
 import { IonModal } from '@ionic/angular';
 import { findBusDetailById } from 'bus-mj';
 
@@ -8,20 +8,16 @@ import { findBusDetailById } from 'bus-mj';
   templateUrl: './modal-bus.component.html',
   styleUrls: ['./modal-bus.component.scss'],
 })
-export class ModalBusComponent  implements OnInit {
-
-  @Input() bus: {
-    BUS_ID: string,
-    ROAD: string[]
-  };
+export class ModalBusComponent implements OnInit {
+  @Input() bus: Bus;
   @Input() stopList: string;
   @Input() trigger: string;
-  // public busDetail: Bus;
+  public busDetail: Bus;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
-    // this.busDetail = busAt(this.bus.BUS_ID);
+    this.busDetail = findBusDetailById(this.bus.id);
   }
 
   colorOf(propriety: string) {
@@ -37,7 +33,7 @@ export class ModalBusComponent  implements OnInit {
       case 'white':
         return 'blanc';
       default:
-        return "toute couleur";
+        return 'toute couleur';
     }
   }
 
@@ -58,5 +54,17 @@ export class ModalBusComponent  implements OnInit {
     }
   }
 
-
+  getOperatorColor(operator: string): string {
+    const map: { [k: string]: string } = {
+      MAMI: '#1E88E5',
+      'KOFIBE/ KOFIMARE': '#009688',
+      MAHATSINJO: '#8E24AA',
+      TAMBATRA: '#43A047',
+      'NY ANTSIKA': '#E53935',
+      AMBONDRONA: '#FFB300',
+      MIRAY: '#00ACC1',
+      AINA: '#D81B60',
+    };
+    return map[operator] || '#607D8B';
+  }
 }
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,18 +1,24 @@
 import { Component } from '@angular/core';
-import { findBusByStopLabel, findBusByTwoStop, findStopAll, findBusByTwoStopLabel } from 'bus-mj';
+import {
+  findBusByStopLabel,
+  findBusByTwoStop,
+  findStopAll,
+  findBusByTwoStopLabel,
+} from 'bus-mj';
 import { Stop, Bus } from '../interface/bus';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
-  styleUrls: ['tab2.page.scss']
+  styleUrls: ['tab2.page.scss'],
 })
 export class Tab2Page {
-
   public allStop: Stop[];
   public result: Bus[] = [];
   public depart: string = 'c';
   public fin: string = 'c';
+  public departOld: string = 'c';
+  public finOld: string = 'c';
   public isShowEmpty: boolean = false;
   public valueSearch: string | null;
   public valueSearch2: string | null;
@@ -27,9 +33,10 @@ export class Tab2Page {
 
   public set stop_1(value: string) {
     this.valueSearch = value;
+    this.depart = this.valueSearch ? this.valueSearch : 'c';
     this.isShowStopHelp = true;
     this.stopFiltered = this.filterStop(this.valueSearch);
-    if(this.valueSearch.length == 0) {
+    if (this.valueSearch.length == 0 || this.stopFiltered.length == 0) {
       this.isShowStopHelp = false;
     }
   }
@@ -40,31 +47,35 @@ export class Tab2Page {
 
   public set stop_2(value: string) {
     this.valueSearch2 = value;
+    this.fin = this.valueSearch2 ? this.valueSearch2 : 'c';
     this.isShowStopHelp2 = true;
     this.stopFiltered = this.filterStop(this.valueSearch2);
-    if(this.valueSearch2.length == 0) {
+    if (this.valueSearch2.length == 0 || this.stopFiltered.length == 0) {
       this.isShowStopHelp2 = false;
     }
   }
 
-  constructor(
-  ) {}
+  constructor() {}
 
   ngOnInit() {
-    this.allStop = findStopAll().sort((a, b) => a.label!.localeCompare(b.label!));
+    this.allStop = findStopAll().sort((a, b) =>
+      a.label!.localeCompare(b.label!)
+    );
   }
 
   findBus() {
-    if(this.depart == 'c' || this.fin == 'c') {
-      alert("Choisissez l\'arrêt de départ et celui d'arrivée.");
-    }else if(this.depart == this.fin){
+    if (this.depart == 'c' || this.fin == 'c') {
+      alert("Choisissez l'arrêt de départ et celui d'arrivée.");
+    } else if (this.depart == this.fin) {
       alert('Les deux arrêts doivent être différents.');
-    }else{
+    } else {
       console.log(this.depart, this.fin);
-      
+      this.departOld = this.depart;
+      this.finOld = this.fin;
       this.isShowEmpty = false;
       this.result = findBusByTwoStopLabel(this.depart, this.fin);
-      if(!this.result[0]) {
+      console.log('result: ', this.result);
+      if (!this.result[0]) {
         this.isShowEmpty = true;
       }
     }
@@ -73,16 +84,24 @@ export class Tab2Page {
   busLabel(busId: string) {
     // return busAt(busId);
   }
-  
+
   stopLabel(stopId: string) {
     // return getStopLabel(stopId);
   }
 
   filterStop(value: string) {
-    return this.allStop.filter((e) => e.label!.toLowerCase().includes(value.toLowerCase()));
+    const seen = new Set<string>();
+    return this.allStop.filter((e) => {
+      const label = e.label!.toLowerCase();
+      if (!label.includes(value.toLowerCase())) return false;
+      if (seen.has(label)) return false;
+      seen.add(label);
+      return true;
+    });
   }
 
   choisir(label: string) {
+    this.result = [];
     this.valueSearch = this.allStop.filter((e) => e.label == label)[0].label;
     this.depart = this.valueSearch ? this.valueSearch : 'c';
     this.isShowStopHelp = false;
@@ -90,6 +109,7 @@ export class Tab2Page {
   }
 
   choisir2(label: string) {
+    this.result = [];
     this.valueSearch2 = this.allStop.filter((e) => e.label == label)[0].label;
     this.fin = this.valueSearch2 ? this.valueSearch2 : 'c';
     this.isShowStopHelp2 = false;
@@ -103,5 +123,4 @@ export class Tab2Page {
   onBlur(inputNumber: number) {
     console.log('perdu le focus: ', inputNumber);
   }
-
 }
