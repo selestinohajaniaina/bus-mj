4b17e98ab5708faff75329add89df1abf8e9727b
selestino <selestinoolivier@gmail.com>
2026-07-23 02:51:24 +0300

    @selestinohajaniaina add search

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -10,23 +10,22 @@ import * as turf from '@turf/turf';
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
-  styleUrls: ['tab3.page.scss']
+  styleUrls: ['tab3.page.scss'],
 })
 export class Tab3Page {
-
   private map: maplibregl.Map;
   private mapCenter: Coordinates;
   private mapZoom: number = 13;
   private mapStyleUrl: string = 'https://tiles.openfreemap.org/styles/positron';
   private haveGPSPermission: boolean = false;
-  private myPosition: MapMarker;
   private mySpeed: number = 0;
 
+  public myPosition: MapMarker;
   public allStop: Stop[];
 
   constructor() {
-    const theme = localStorage.getItem("theme");
-    if (theme == "dark") {
+    const theme = localStorage.getItem('theme');
+    if (theme == 'dark') {
       this.mapStyleUrl = 'https://tiles.openfreemap.org/styles/dark';
     } else {
       this.mapStyleUrl = 'https://tiles.openfreemap.org/styles/positron';
@@ -58,12 +57,11 @@ export class Tab3Page {
         });
       });
     }
-
   }
 
   addMarker(_marker: MapMarker, isMyPosition = false) {
     const marker = new maplibregl.Marker({
-      color: isMyPosition ? '#e74c3c' : '#3FB1CE'
+      color: isMyPosition ? '#e74c3c' : '#3FB1CE',
     })
       .setLngLat([_marker.longitude, _marker.latitude])
       .setPopup(new maplibregl.Popup().setText(_marker.label))
@@ -72,17 +70,27 @@ export class Tab3Page {
 
   setCenter(coordinate: Coordinates, zoom: number = 16, speed: number = 1.5) {
     this.map.flyTo({
-      center: [
-        this.myPosition.longitude,
-        this.myPosition.latitude
-      ],
+      center: [this.myPosition.longitude, this.myPosition.latitude],
       zoom: zoom,
-      speed: speed
+      speed: speed,
     });
   }
 
-  drawDistance(coordinate1: Coordinates, coordinate2: Coordinates, text: string = '') {
-    const routeId = `route-${Math.abs(coordinate1.longitude + coordinate1.latitude + coordinate2.longitude + coordinate2.latitude)}-${text}`
+  drawDistance(
+    coordinate1: Coordinates,
+    coordinate2: Coordinates,
+    text: string = ''
+  ) {
+    const routeId = `route-${Math.abs(
+      coordinate1.longitude +
+        coordinate1.latitude +
+        coordinate2.longitude +
+        coordinate2.latitude
+    )}-${text}`;
+
+    console.log("id route", routeId);
+    
+
     this.map.addSource(routeId, {
       type: 'geojson',
       data: {
@@ -92,10 +100,10 @@ export class Tab3Page {
           type: 'LineString',
           coordinates: [
             [coordinate1.longitude, coordinate1.latitude],
-            [coordinate2.longitude, coordinate2.latitude]
-          ]
-        }
-      }
+            [coordinate2.longitude, coordinate2.latitude],
+          ],
+        },
+      },
     });
 
     this.map.addLayer({
@@ -104,8 +112,8 @@ export class Tab3Page {
       source: routeId,
       paint: {
         'line-color': '#0d6efd',
-        'line-width': 4
-      }
+        'line-width': 4,
+      },
     });
 
     this.map.addLayer({
@@ -116,13 +124,13 @@ export class Tab3Page {
         'symbol-placement': 'line',
         'text-field': text,
         'text-size': 14,
-        'text-offset': [0, -1]
+        'text-offset': [0, -1],
       },
       paint: {
         'text-color': '#757575',
         'text-halo-color': '#fff',
-        'text-halo-width': 2
-      }
+        'text-halo-width': 2,
+      },
     });
   }
 
@@ -135,30 +143,43 @@ export class Tab3Page {
   }
 
   async getWebPosition() {
-    const status = await navigator.permissions.query({
-      name: 'geolocation'
-    });
-    if (status.state == "granted") {
-      navigator.geolocation.getCurrentPosition(
-        (position) => {
-          this.haveGPSPermission = true;
-          this.myPosition = { longitude: position.coords.longitude, latitude: position.coords.latitude, label: "Vous etes ici" };
-          this.mySpeed = position.coords.speed ?? 0;
-          this.addMarker(this.myPosition, true);
-          this.setCenter(this.myPosition);
-          const stopNearsMe = this.getNearsStop(this.myPosition);
-          stopNearsMe.map((e) => {
-            this.drawDistance(this.myPosition, { longitude: e.lon, latitude: e.lat }, `${Math.round(e.distance)} m`);
-          });
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        this.haveGPSPermission = true;
+        this.myPosition = {
+          longitude: position.coords.longitude,
+          latitude: position.coords.latitude,
+          label: 'Vous etes ici',
+        };
+        this.mySpeed = position.coords.speed ?? 0;
+        this.addMarker(this.myPosition, true);
+        this.setCenter(this.myPosition);
+        const stopNearsMe = this.getNearsStop(this.myPosition);
 
-        },
-        (error) => {
-          this.haveGPSPermission = false;
-        },
-        {
-          enableHighAccuracy: true
-        }
-      );
+        this.map.on('load', () => {
+          stopNearsMe.map((e) => {
+            this.drawDistance(
+              this.myPosition,
+              { longitude: e.lon, latitude: e.lat },
+              `${Math.round(e.distance)}m`
+            );
+          });
+        });
+      },
+      (error) => {
+        this.haveGPSPermission = false;
+      },
+      {
+        enableHighAccuracy: true,
+      }
+    );
+  }
+
+  setToMyPosition() {
+    if (this.myPosition) {
+      this.setCenter(this.myPosition);
+    } else {
+      this.getGPS;
     }
   }
 
@@ -171,42 +192,46 @@ export class Tab3Page {
       this.haveGPSPermission = reqPermissions.location === 'granted';
     }
     if (this.haveGPSPermission) {
-      const position = await Geolocation.getCurrentPosition({ enableHighAccuracy: true });
-      this.myPosition = { longitude: Number(position.coords.longitude), latitude: Number(position.coords.latitude), label: "Vous etes ici" };
+      const position = await Geolocation.getCurrentPosition({
+        enableHighAccuracy: true,
+      });
+      this.myPosition = {
+        longitude: Number(position.coords.longitude),
+        latitude: Number(position.coords.latitude),
+        label: 'Vous etes ici',
+      };
       this.mySpeed = position.coords.speed ?? 0;
       this.addMarker(this.myPosition, true);
       this.setCenter(this.myPosition);
       const stopNearsMe = this.getNearsStop(this.myPosition);
-      stopNearsMe.map((e) => {
-        this.drawDistance(this.myPosition, { longitude: e.lon, latitude: e.lat }, `${Math.round(e.distance)} m`);
+      this.map.on('load', () => {
+        stopNearsMe.map((e) => {
+          this.drawDistance(
+            this.myPosition,
+            { longitude: e.lon, latitude: e.lat },
+            `${Math.round(e.distance)}m`
+          );
+        });
       });
     }
   }
 
   getNearsStop(coordinate: Coordinates) {
-    const myPoint = turf.point([
-      coordinate.longitude,
-      coordinate.latitude
-    ]);
+    const myPoint = turf.point([coordinate.longitude, coordinate.latitude]);
 
     const sortedStops = this.allStop
-      .map(stop => ({
+      .map((stop) => ({
         ...stop,
-        distance: turf.distance(
-          myPoint,
-          turf.point([stop.lon, stop.lat]),
-          { units: 'meters' }
-        )
+        distance: turf.distance(myPoint, turf.point([stop.lon, stop.lat]), {
+          units: 'meters',
+        }),
       }))
       .sort((a, b) => a.distance - b.distance);
 
     const nearbyStops = sortedStops
-      .filter(stop => stop.distance <= 300)
+      .filter((stop) => stop.distance <= 300)
       .slice(0, 10);
 
-    return nearbyStops.length > 0
-      ? nearbyStops
-      : sortedStops.slice(0, 1);
+    return nearbyStops.length > 0 ? nearbyStops : sortedStops.slice(0, 1);
   }
-
 }
