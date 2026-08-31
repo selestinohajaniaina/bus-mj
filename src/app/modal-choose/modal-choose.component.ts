157c00f7a48fd2a551259bfed0b42842ff96f8d1
selestino <selestinoolivier@gmail.com>
2026-08-31 23:16:10 +0300

    @selestinohajaniaina feat:Add OSM search to find bus with place

--- src/app/modal-choose/modal-choose.component.ts
+++ src/app/modal-choose/modal-choose.component.ts
@@ -1,6 +1,11 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { StorageService } from '../service/storage.service';
-import { MapMarker, OSMResult, OSMResultStored } from '../interface/Map';
+import {
+  Coordinates,
+  MapMarker,
+  OSMResult,
+  OSMResultStored,
+} from '../interface/Map';
 import { Router } from '@angular/router';
 import { ViewWillEnter } from '@ionic/angular';
 import { LocalisationService } from '../service/localisation.service';
@@ -8,6 +13,8 @@ import { Stop } from '../interface/bus';
 import * as turf from '@turf/turf';
 import { findStopAll } from 'bus-mj';
 import { TranslateService } from '@ngx-translate/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-modal-choose',
@@ -24,8 +31,11 @@ export class ModalChooseComponent implements OnInit {
   public isStorageEmpty: boolean = true;
   public myPositionOSM: OSMResultStored;
   private allStop: Stop[] = [];
+  private url: string = environment.searchUrl;
 
-  public get querySearch(): string | null {
+  public chargeShow: boolean = false;
+
+  public get querySearch(): string {
     return this.valueSearch;
   }
 
@@ -33,12 +43,6 @@ export class ModalChooseComponent implements OnInit {
     this.valueSearch = value;
     if (!value.trim()) {
       this.loadPlaces();
-    } else {
-      this.placeResult = this.storage.getMyPlacesByName(value);
-
-      const stopResult = this.filterStop(value);
-      const OsmStopResult = this.stopToOsmResultStored(stopResult);
-      OsmStopResult.map((e: OSMResultStored) => this.placeResult.push(e));
     }
   }
 
@@ -46,7 +50,8 @@ export class ModalChooseComponent implements OnInit {
     private storage: StorageService,
     private router: Router,
     private localisation: LocalisationService,
-    private translate: TranslateService
+    private translate: TranslateService,
+    private http: HttpClient
   ) {}
 
   ngOnInit() {
@@ -55,7 +60,7 @@ export class ModalChooseComponent implements OnInit {
 
   loadPlaces() {
     this.placeResult = this.storage.getAllMyPlaces();
-    if(this.placeResult.length > 0) {
+    if (this.placeResult.length > 0) {
       this.isStorageEmpty = false;
     } else {
       this.isStorageEmpty = true;
@@ -143,4 +148,127 @@ export class ModalChooseComponent implements OnInit {
     return this.localisation.getDisplayDistance(distance);
   }
 
+  /**
+   * Recherche un peu de resultat enligne si un acces internet est disponible
+   * @returns
+   */
+  find() {
+    if (!this.querySearch.trim()) {
+      this.loadPlaces();
+    } else {
+      this.placeResult = this.storage.getMyPlacesByName(this.querySearch);
+      const stopResult = this.filterStop(this.querySearch);
+      const OsmStopResult = this.stopToOsmResultStored(stopResult);
+      OsmStopResult.map((e: OSMResultStored) => this.placeResult.push(e));
+      try {
+        this.searchToNet();
+      } catch (error) {
+        return;
+      }
+    }
+  }
+
+  searchToNet() {
+    this.chargeShow = true;
+    this.http
+      .get(this.url, {
+        params: {
+          q: this.querySearch,
+          format: 'json',
+          countrycodes: 'mg',
+          viewbox: '46.20,-15.60,46.45,-15.82',
+          bounded: '1',
+        },
+      })
+      .subscribe({
+        next: (result: any) => {
+          this.chargeShow = false;
+          result.map((e: OSMResult) => {
+            const distance = this.getDistance(
+              turf.point([
+                Number(this.myPositionOSM.lon),
+                Number(this.myPositionOSM.lat),
+              ]),
+              turf.point([Number(e.lon), Number(e.lat)])
+            );
+            const nearStop = this.getNearsStop({
+              longitude: Number(e.lon),
+              latitude: Number(e.lat),
+            });
+            const nearStopLength = nearStop.length;
+            const elementOSMResult = {
+              ...e,
+              distance,
+              display_distance: this.localisation.getDisplayDistance(distance),
+              nearStop,
+              nearStopLength,
+              saved_at: new Date().toISOString(),
+            };
+            if (!this.isSaved(elementOSMResult)) {
+              this.placeResult.push(elementOSMResult);
+              this.save(elementOSMResult);
+            }
+          });
+        },
+        error: (err: any) => {
+          this.chargeShow = false;
+        },
+      });
+  }
+
+  getNearsStop(coordinate: Coordinates) {
+    const searchedPoint = turf.point([
+      coordinate.longitude,
+      coordinate.latitude,
+    ]);
+    const sortedStops = this.allStop
+      .map((stop) => {
+        const distance = this.getDistance(
+          searchedPoint,
+          turf.point([stop.lon, stop.lat])
+        );
+        return {
+          ...stop,
+          distance,
+          display_distance: this.localisation.getDisplayDistance(distance),
+        };
+      })
+      .sort((a, b) => a.distance - b.distance);
+    const nearbyStops = sortedStops
+      .filter((stop) => stop.distance <= 250)
+      .slice(0, 10);
+    return nearbyStops.length > 0 ? nearbyStops : sortedStops.slice(0, 1);
+  }
+
+  stopToOsmResult(stop: Stop[]): OSMResult[] {
+    return stop.map((st) => {
+      const distance = this.getDistance(
+        turf.point([
+          Number(this.myPositionOSM.lon),
+          Number(this.myPositionOSM.lat),
+        ]),
+        turf.point([st.lon, st.lat])
+      );
+      return {
+        osm_id: st.id,
+        display_name: String(st.label),
+        name: String(st.label),
+        lon: st.lon,
+        lat: st.lat,
+        type: st.type,
+        distance,
+        display_distance: this.localisation.getDisplayDistance(distance),
+        nearStop: [],
+        nearStopLength: 0,
+      };
+    });
+  }
+
+  isSaved(place: OSMResult): boolean {
+    return this.storage.getAllMyPlaces().some((p) => p.osm_id === place.osm_id);
+  }
+
+  save(place: OSMResult) {
+    this.storage.addMyPlace(place);
+  }
 }
